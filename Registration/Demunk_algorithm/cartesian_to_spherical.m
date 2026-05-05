82fc28749932812eaf6a49f91f27984ba1e7885c
Mohammadreza <89751210+mohammadrezarezaeid@users.noreply.github.com>
2026-05-04 23:31:55 -0600

    Delete Registration/Demunk_algorithm directory

--- Registration/Demunk_algorithm/cartesian_to_spherical.m
+++
@@ -1,8 +0,0 @@
-function spherical = cartesian_to_spherical(cartesian)
-% cartesian: Nx3 matrix, x,y, and z
-% convert to spherical coordinates: Nx2 matrix, theta and phi
-
-theta = atan2(cartesian(:,2),cartesian(:,1));
-phi = atan2(sqrt(sum(cartesian(:,1:2).^2,2)),cartesian(:,3));
-
-spherical = [theta phi];
--- Registration/Demunk_algorithm/load_electrodes_file.m
+++
@@ -1,7 +0,0 @@
-function data = load_electrodes_file(filename)
-
-    s = load(filename);
-    f = fieldnames(s);
-    data = s.(f{1});
-
-end
